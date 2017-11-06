525179da5b3109ac228e5f916ed3e3de66f11bf3
yamacho <yamacho@users.noreply.github.com>
2017-11-06 22:53:44 +0900

    ランダムな整数を表示させよう
    * ランダムな整数の表示

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@
   var btn = document.getElementById('btn');
 
   btn.addEventListener('click', function() {
-    this.textContent = 'hit!';
+    var n = Math.floor(Math.random() * 3);
+
+    this.textContent = n;
   });
 
   btn.addEventListener('mousedown', function() {
