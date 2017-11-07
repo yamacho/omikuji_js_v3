6e8db7e6ec21893f5e83852daebb946bffb3a1f1
yamacho <yamacho@users.noreply.github.com>
2017-11-07 21:12:34 +0900

    確率を変更してみよう
    * 確率の変更

--- js/main.js
+++ js/main.js
@@ -4,10 +4,15 @@
   var btn = document.getElementById('btn');
 
   btn.addEventListener('click', function() {
-    var results = ['大吉', '中吉', '凶', '末吉'];
-    var n = Math.floor(Math.random() * results.length);
+    var n = Math.random();
 
-    this.textContent = results[n];
+    if (n < 0.05) {
+      this.textContent = '大吉'; // 5 %
+    } else if (n < 0.2) {
+      this.textContent = '中吉'; // 15 %
+    } else {
+      this.textContent = '凶';  // 80 %
+    }
   });
 
   btn.addEventListener('mousedown', function() {
